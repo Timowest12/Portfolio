634fd00c7d09034869c9032af200d01926701811
Timowest12 <timowester@gmail.com>
2021-10-04 15:36:39 +0200

    Add eventlisteners

--- scripts/js.js
+++ scripts/js.js
@@ -1,1 +1,8 @@
-function mobileMenu() { document.querySelector('.mobilemenu').classList.add('mobilemenuopen'); } function closemobilemenu() { document.querySelector('.mobilemenu').classList.remove('mobilemenuopen'); }
+function mobileMenu() {
+  document.querySelector(".mobilemenu").classList.add("mobilemenuopen");}
+function closemobilemenu() {
+  document.querySelector(".mobilemenu").classList.remove("mobilemenuopen");
+}
+document.querySelector("#mobilemenuicon").addEventListener("click",mobileMenu)
+document.querySelector("#closemobilemenu").addEventListener("click",closemobilemenu)
+
