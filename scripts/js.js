66f110b94ab1400db50c7a7d283e60abbb2fb1af
Timowest12 <timowester@gmail.com>
2021-10-05 11:42:11 +0200

    Add links

--- scripts/js.js
+++ scripts/js.js
@@ -4,7 +4,22 @@ function mobileMenu() {
 function closemobilemenu() {
   document.querySelector('.mobilemenu').classList.remove('mobilemenuopen');
 }
+function navtoportfolio() {
+  window.scrollTo(0, document.getElementById('projectCards').offsetTop);
+  closemobilemenu();
+}
+function navtoabout() {
+  window.scrollTo(0, document.getElementById('about').offsetTop);
+  closemobilemenu();
+}
+function navtocontact() {
+  window.scrollTo(0, document.getElementById('contact').offsetTop);
+  closemobilemenu();
+}
 document.querySelector('#mobilemenuicon').addEventListener('click', mobileMenu);
+document.querySelector('.navtoportfolio').addEventListener('click', navtoportfolio);
+document.querySelector('.navtoabout').addEventListener('click', navtoabout);
+document.querySelector('.navtocontact').addEventListener('click', navtocontact);
 document
   .querySelector('.closemobilemenu')
   .addEventListener('click', closemobilemenu);
