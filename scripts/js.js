94497e832bde19207adefebaf1cb2837a985a110
Timowest12 <timowester@gmail.com>
2021-10-05 13:22:05 +0200

    Add test objects

--- scripts/js.js
+++ scripts/js.js
@@ -1,3 +1,17 @@
+var projectsarray = [
+  {'name':'test','description':'test test test','mainimage':'project1img.png','technologies':['tech1','tech2','tech3'],'livelink':'test','sourcelink':'test'},
+  {'name':'test2','description':'test2','mainimage':'project2img.png','technologies':['tech3','tech4','tech4'],'livelink':'test2','sourcelink':'test2'}
+];
+for(project in projectsarray){
+  project = projectsarray[project];
+  var technologieshtml = '';
+  for(technologie in project.technologies){
+    technologie = project.technologies[technologie];
+    technologieshtml += "<li>"+technologie+"</li>";
+  }
+  console.log(technologieshtml);
+  document.querySelector('#cardContainer').innerHTML += "<div class='card'><div class='leftcard'><div class='imgplaceholder'><img class='projectimg' src='/assets/images/"+project.mainimage+"'></div></div><div class='rightcard'><h3 class='cardTxt'>"+project.name+"</h3><p class='cardText'>"+project.description+"</p><div><ul class='skillsbar'>"+technologieshtml+"</ul></div><div class='probtn'><button class='projectbtn'>see project</button></div></div></div>";
+}
 function mobileMenu() {
   document.querySelector('.mobilemenu').classList.add('mobilemenuopen');
 }
