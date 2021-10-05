b15e82859638bfdfa2c0d76eece1d37ec6d517b0
Timowest12 <timowester@gmail.com>
2021-10-05 14:59:08 +0200

    Add function project details

--- scripts/js.js
+++ scripts/js.js
@@ -1,43 +1,61 @@
 const projectsarray = [
   {
-    name: 'test', description: 'test test test', mainimage: 'project1img.png', technologies: ['tech1', 'tech2', 'tech3'], livelink: 'test', sourcelink: 'test',
+    name: "test",
+    description: "test test test",
+    mainimage: "project1img.png",
+    technologies: ["tech1", "tech2", "tech3"],
+    livelink: "test",
+    sourcelink: "test",
   },
   {
-    name: 'test2', description: 'test2', mainimage: 'project2img.png', technologies: ['tech3', 'tech4', 'tech4'], livelink: 'test2', sourcelink: 'test2',
+    name: "test2",
+    description: "test2",
+    mainimage: "project2img.png",
+    technologies: ["tech3", "tech4", "tech4"],
+    livelink: "test2",
+    sourcelink: "test2",
   },
 ];
 for (let project in projectsarray) {
+  let index = project;
   project = projectsarray[project];
-  let technologieshtml = '';
+  let technologieshtml = "";
   for (let technologie in project.technologies) {
     technologie = project.technologies[technologie];
     technologieshtml += `<li>${technologie}</li>`;
   }
-  console.log(technologieshtml);
-  document.querySelector('#cardContainer').innerHTML += `<div class='card'><div class='leftcard'><div class='imgplaceholder'><img class='projectimg' src='/assets/images/${project.mainimage}'></div></div><div class='rightcard'><h3 class='cardTxt'>${project.name}</h3><p class='cardText'>${project.description}</p><div><ul class="skillsbar">${technologieshtml}</ul></div><div class='probtn'><button class='projectbtn'>see project</button></div></div></div>`;
+  console.log(index);
+  document.querySelector(
+    "#cardContainer"
+  ).innerHTML += `<div class='card'><div class='leftcard'><div class='imgplaceholder'><img class='projectimg' src='/assets/images/${project.mainimage}'></div></div><div class='rightcard'><h3 class='cardTxt'>${project.name}</h3><p class='cardText'>${project.description}</p><div><ul class="skillsbar">${technologieshtml}</ul></div><div class='probtn'><button onclick='projectdetails(${index})' class='projectbtn'>see project</button></div></div></div>`;
 }
 function mobileMenu() {
-  document.querySelector('.mobilemenu').classList.add('mobilemenuopen');
+  document.querySelector(".mobilemenu").classList.add("mobilemenuopen");
 }
 function closemobilemenu() {
-  document.querySelector('.mobilemenu').classList.remove('mobilemenuopen');
+  document.querySelector(".mobilemenu").classList.remove("mobilemenuopen");
 }
 function navtoportfolio() {
-  window.scrollTo(0, document.getElementById('projectCards').offsetTop);
+  window.scrollTo(0, document.getElementById("projectCards").offsetTop);
   closemobilemenu();
 }
 function navtoabout() {
-  window.scrollTo(0, document.getElementById('about').offsetTop);
+  window.scrollTo(0, document.getElementById("about").offsetTop);
   closemobilemenu();
 }
 function navtocontact() {
-  window.scrollTo(0, document.getElementById('contact').offsetTop);
+  window.scrollTo(0, document.getElementById("contact").offsetTop);
   closemobilemenu();
 }
-document.querySelector('#mobilemenuicon').addEventListener('click', mobileMenu);
-document.querySelector('.navtoportfolio').addEventListener('click', navtoportfolio);
-document.querySelector('.navtoabout').addEventListener('click', navtoabout);
-document.querySelector('.navtocontact').addEventListener('click', navtocontact);
+function projectdetails(index){
+console.log(projectsarray[index])
+}
+document.querySelector("#mobilemenuicon").addEventListener("click", mobileMenu);
 document
-  .querySelector('.closemobilemenu')
-  .addEventListener('click', closemobilemenu);
+  .querySelector(".navtoportfolio")
+  .addEventListener("click", navtoportfolio);
+document.querySelector(".navtoabout").addEventListener("click", navtoabout);
+document.querySelector(".navtocontact").addEventListener("click", navtocontact);
+document
+  .querySelector(".closemobilemenu")
+  .addEventListener("click", closemobilemenu);
